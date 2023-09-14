d11cee6fafce33e4a9106f5aadcd7c8f0f931bfe
sivak243 <sivakumar243@yahoo.co.in>
2023-09-14 09:59:20 +0000

    modified the choice code

--- 08_Choice.sh
+++ 08_Choice.sh
@@ -5,13 +5,21 @@ echo " Plesae select from the below mentioned Options."
 echo "a. Print todays date"
 echo "b. Display the current path"
 echo "c. Display the list of files"
+echo "d. Display the memory space"
+echo "e. Display process with high CPU utilization"
+echo "f. Display the ports information"
 
 read -p " Enter your cholice : " choice
 
 case $choice in
-	a)date;;
+	a)      echo "Todays date is : "  
+		date
+		echo "************";;        # ;; will acts like a break statement in switch class
 	b)pwd;;
 	c)ls -ltr;;
-	*)echo "Invalid Option";;
+	d)df -h;;
+	e)top;;
+	f)ss -tulp;;
+	*)echo "Invalid Option"
 esac
 
