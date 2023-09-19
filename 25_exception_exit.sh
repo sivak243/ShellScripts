c50e9736bdfea4585cabcb8903254a0f6a5843af
sivak243 <sivakumar243@yahoo.co.in>
2023-09-19 13:34:50 +0000

    21-26 Script commit

--- 25_exception_exit.sh
+++ 25_exception_exit.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -e
+
 no=3
+
+pwd
+cd /root
+echo "testing message"
 
 for i in 1 2 3 4 5 6
 do 
--- 26_nohup_Backgroud.sh
+++ 26_nohup_Backgroud.sh
@@ -3,12 +3,15 @@
 for i in 1 2 3 4 5
 do
 	echo "Value is $i"
+	sleep 5s
 done
 
+#nohup ./filename.sh &
 
 for i in siva kumar kommoju
 do 
 	echo "Value is $i"
+	sleep 2s
 done
 
 
