803c596d972cd976b6bff2b0b227d15607ebf8ff
sivak243 <ubuntu@ip-172-31-6-143.ap-south-1.compute.internal>
2023-09-23 11:31:26 +0000

    Added RAM check

--- project2-RAM-MemoryCheck/RAM-Check.sh
+++ project2-RAM-MemoryCheck/RAM-Check.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #set -x
 
-x=$(free -h | grep "Mem:" | awk '{print $4}')
+x=$(free -h | grep "Mem:" | awk '{print $4}' | tr -d 'Mi')
 
-echo "free memory space is : $x"
+echo "free memory space is : ${x}Mib"
 
-if ( $x -le 120mi )
+if [ $x -le 100 ] 
 then
-	echo "Less RAM Memory $x "
+    echo "Less RAM Memory ${x}Mib "
 else
-	echo "Current RAM Free Memory is : $x "
-	echo "Your are having good RAM Memory"
+    echo "Current RAM Free Memory is : $x "
+    echo "Your are having good RAM Memory"
 fi
-
