483b7233a6fa099c074fdb76efdf45f1e73a5820
root <root@ip-172-31-6-143.ap-south-1.compute.internal>
2023-09-24 06:42:44 +0000

    update proj1

--- project1-NodeStatus/nodehealth.sh
+++ project1-NodeStatus/nodehealth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-set -x
+#set -e
+#set -x
 
 echo " Plesae select from the below mentioned Options."
 
@@ -20,25 +20,27 @@ case $choice in
 		ps -ef;;
 	b)top;;
 	c)
-	  read -p "Please enter the process id to kill" processid
+	  read -p "Please enter the process id to kill  :  " processid
 
-	  if [ $processid -gt 0 ]
-	  then  
-		x=$(top -b -n 1 | grep " 36 " | awk -F" " '{print $12}')
+	  if [ $processid -gt 0 ]; then  
+		#x=$(top -b -n 1 | grep $processid | awk -F" " '{print $12}')
+		x=$(top -b -n 1 | awk -F" " '{print $1 , $12}' |grep -w $processid | awk -F" " '{print $2}')
+
 	        echo "You are deleting the process : ${x} "	
 		
                 read -p "Are you sure to delete the process : $x ?? (Y/N) :" confirm
 
-		if [$confirm -eq "Y"]; 
-		then
-		     kill -9 $processid     # Terminate the proceie id
-		else
-		     echo "We are not Terminating the process "
-		if
-	  else
-	  echo "Please enter valid process id to kill....."	 
-	  fi
-	  ;;
+                if [[ "$confirm" == "Y" || "$confirm" == "y" ]]; then
+                     kill -9 $processid     # Terminate the proceie id
+                else
+                     echo "We are not Terminating the process "
+		fi
+          else
+                 echo "Please enter valid process id to kill....."
+          fi;;
+        d)df -h;;
+        e)free;;
+        f)nproc;;
 	d)df -h;;
 	e)free;;
 	f)nproc;;
